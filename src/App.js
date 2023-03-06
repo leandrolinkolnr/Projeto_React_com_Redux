d6f8b9378a861a24e5a542b8503c9da743c9271b
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2023-03-06 15:35:15 -0300

    Escolhendo o estilo dos cards

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
     <div className="App">
       <h1> Exercicio React - Redux (Simples)</h1>
       <div className='linha'>
-          <Card/>
+          <Card title="Card 1" red>X</Card>
+          <Card title="Card 2" green>X</Card>
       </div>
       <div className='linha'>
-          <Card/>
+          <Card title="Card 3" blue>Y</Card>
+          <Card title="Card 4" purple>X</Card>
       </div>
     </div>
   );
